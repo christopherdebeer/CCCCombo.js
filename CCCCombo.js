3d30f482e595890fa375575c67966a1235c21eff
Christopher de Beer <christopherdebeer@gmail.com>
2012-04-04 12:10:52 +0100

    progress (only for mobile)

--- CCCCombo.js
+++ CCCCombo.js
@@ -1,9 +1,41 @@
+(function ($){
+  var check=false, isRelative=true;
+
+  $.elementFromPoint = function(x,y)
+  {
+    if(!document.elementFromPoint) return null;
+
+    if(!check)
+    {
+      var sl;
+      if((sl = $(document).scrollTop()) >0)
+      {
+       isRelative = (document.elementFromPoint(0, sl + $(window).height() -1) == null);
+      }
+      else if((sl = $(document).scrollLeft()) >0)
+      {
+       isRelative = (document.elementFromPoint(sl + $(window).width() -1, 0) == null);
+      }
+      check = (sl>0);
+    }
+
+    if(!isRelative)
+    {
+      x += $(document).scrollLeft();
+      y += $(document).scrollTop();
+    }
+
+    return document.elementFromPoint(x,y);
+  }	
+
+})(jQuery);
+
 (function($) {
 	$.fn.CCCCombo = function(options) {
 		
 		options = $.extend({
 			hackTheCSS: false,
-			comboTimeout: 500,
+			comboTimeout: 1000,
 			tapTimeout: 100,
 			onCombo: $.noop
 		}, options);
@@ -11,10 +43,41 @@
 
 		var CCCCombos = [
 			{
-				id: "all",
-				desc: "Generous regex",
+				id: "rH2",
+				desc: "Reverse RoundHouse!",
 				moves: [
-					{dir: [/[ud]?l+[ud]?r+[ud]?l+/gi]}
+					{dir: ["ldru", "ruld"]}
+				]
+			},
+			{
+				id: "rH",
+				desc: "RoundHouse!",
+				moves: [
+					{dir: ["lurd","rdlu"]}
+				]
+			},
+			{
+				id: "slr",
+				desc: "Simple left right",
+				moves: [
+					{dir: ["tap"], areas: ["mL"]},
+					{dir: ["tap"], areas: ["mR"]}
+				]
+			},
+			{
+				id: "ffx",
+				desc: "ForwardForward Punch",
+				moves: [
+					{dir: ["r"]},
+					{dir: ["r"]},
+					{dir: ["tap"], areas: ["mR"]}
+				]
+			},
+			{
+				id: "all",
+				desc: "Generous regex", 
+				moves: [
+					{dir: [/[rlud]*]/]}
 				]
 			},
 			{
@@ -96,18 +159,28 @@
 				
 
 				var matchCount = 0,
-					matchedMoves = [];
+					matchedMoves = [],
+					numMoves = lastMove.length;
 
 				for (m in lastMove) {
 
 					console.log( m, " move: ", lastMove[0])
 
 					// check is move [m] has matching area
-					var matchingArea = _.filter(CCCCombos, function(comb){
-						if (typeof comb.moves[m] !== 'undefined') return ((typeof comb.moves[m].areas === 'undefined') || (_.indexOf(comb.moves[m].areas, lastMove[m].id) !== -1));
+					var matchingArea = _.filter(CCCCombos, function(comb) {
+
+						if (typeof comb.moves[m] !== 'undefined') {
+
+							var areas = comb.moves[m].areas,
+								noArea = typeof areas === 'undefined',
+								areaMatch = _.indexOf(areas, lastMove[m].id) !== -1;
+
+							return noArea || areaMatch;
+						}
+						
 					})
 
-					console.log("possible matches: ", matchingArea);
+					//console.log("possible matches: ", matchingArea);
 
 
 					// check if move [m] has matching dir
@@ -116,27 +189,38 @@
 
 							var dirs = comb.moves[m].dir,
 								noDir = typeof dirs === 'undefined',
-								dirMatch = _.indexOf(dirs, lastMove[m].dir) !== -1;
+								dirMatch = _.indexOf(dirs, lastMove[m].dir) !== -1,
 								dirRegex = _.filter(dirs, function (dir) {
 									if (dir instanceof RegExp) return dir.test(lastMove[m].dir);
 									else return false;
 								}) > 0;
 
+								console.log("regex match: " + dirRegex.toString());
+
 							return noDir || dirMatch || dirRegex;
 						}
 					})
 
-					console.log("refined matches: ", matchingDir);
+					//console.log("refined matches: ", matchingDir);
 
 					if (matchingDir.length > 0) matchCount++;
+					if (matchCount === lastMove.length) {
+
+						var numMovesMatch = _.filter(matchingDir, function(move) {
+							return move.moves.length === numMoves;
+						})
+
+						matchedMoves = _.union(matchedMoves, numMovesMatch);
+					}
 				}
 				
 			}
+
+
 			options.onCombo({
 				move 	: lastMove, 
-				// poss 	: _.pluck(matchingArea,"desc").join(","), 
-				// ref 	: _.pluck(matchingDir, "desc").join(","), 
-				match 	: matchCount === lastMove.length,
+				matched : _.pluck(matchedMoves, "desc").join(" | "),
+				match 	: matchedMoves.length > 0,
 				matchCount: matchCount
 			});
 		} 
@@ -161,6 +245,7 @@
 
 			$this.hammer().bind('dragend', function (ev) {
 				
+				console.log("multipoint: ",ev.touches.length > 1);
 				if (lastEl.id !== null) {
 					startComboTimeout();
 					lastMove.push(moveCheck(lastEl));
@@ -174,6 +259,7 @@
 
 			$this.hammer().bind('tap', function (ev) {
 				
+				console.log("multipoint: ",ev.touches.length > 1);
 				if (!tapTimeout) {
 					startTapTimeout();
 					var id = $this.attr('data-combo-id');					
@@ -185,6 +271,7 @@
 
 			$this.hammer().bind('doubletap', function (ev) {
 				
+				console.log("multipoint: " + (ev.touches.length > 1));
 				if (!tapTimeout) {
 					startTapTimeout();
 					var id = $this.attr('data-combo-id');
